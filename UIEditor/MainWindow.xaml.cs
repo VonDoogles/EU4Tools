2ec4499a9168110ef176a518fb9f10dd5e749a41
Mark Trumbull <mark.trumbull@gamemechs.com>
2015-10-23 18:44:31 -0700

    Added zoom support for the Main Canvas view. Added a zoom toolbar.

--- UIEditor/MainWindow.xaml.cs
+++ UIEditor/MainWindow.xaml.cs
@@ -28,6 +28,8 @@ namespace UIEditor
         Image TestImage = null;
         Rectangle SelectedRect = null;
         FileGUI.Element GfxDoc = null;
+        double CurrentZoom = 100.0;
+        bool bProcessZoomSelectionChanged = true;
 
         public MainWindow()
         {
@@ -97,7 +99,7 @@ namespace UIEditor
             SelectedRect.Height = Size.Field<double>( "y" );
             Canvas.SetLeft( SelectedRect, Pos.Field<double>( "x" ) );
             Canvas.SetTop( SelectedRect, Pos.Field<double>( "x" ) );
-            SelectedRect.Fill = Brushes.Yellow;
+            SelectedRect.Fill = Brushes.Transparent;
             SelectedRect.Stroke = Brushes.Black;
 
             PropGrid.ItemsSource = Win.ItemList;
@@ -116,6 +118,29 @@ namespace UIEditor
             {
                 TextView.Select( 0, 0 );
             }
+
+            FileGUI.Element GfxElement = Win.FindFirst( Elem => Elem.Name == "spriteType" );
+
+            if ( GfxDoc != null && GfxElement != null )
+            {
+                char[] ToTrim = { '"' };
+                string SpriteName = GfxElement.Value;//.Trim( ToTrim );
+
+                FileGUI.Element Sprite = GfxDoc.FindFirst( Elem => Elem.Name == "spriteType" && Elem.Field<string>( "name" ) == SpriteName );
+                if ( Sprite != null )
+                {
+                    string SpriteFileName = Sprite.Field<string>( "texturefile" ).Trim( ToTrim );
+                    SpriteFileName = System.IO.Path.Combine( Utils.FindGamePath(), SpriteFileName );
+
+                    DDSImage dds = new DDSImage( File.Open( SpriteFileName, FileMode.Open ) );
+                    TestImage.Source = dds.Bitmap;
+                    TestImage.Visibility = System.Windows.Visibility.Visible;
+                }
+            }
+            else
+            {
+                TestImage.Visibility = System.Windows.Visibility.Hidden;
+            }
         }
 
         private void TextView_PreviewMouseWheel( object sender, MouseWheelEventArgs e )
@@ -130,6 +155,228 @@ namespace UIEditor
                 });
                 TextView.Dispatcher.BeginInvoke( DelayFocus, DispatcherPriority.Input );
             }
+        }
+
+        bool ContainsDigitsOnly( string Text )
+        {
+            bool Result = true;
+            foreach (char Ch in Text)
+            {
+                Result &= Char.IsDigit( Ch );
+            }
+            return Result;
+        }
+
+        private void TextResX_PreviewTextInput( object sender, TextCompositionEventArgs e )
+        {
+            e.Handled = !ContainsDigitsOnly( e.Text );
+            base.OnPreviewTextInput( e );
+        }
+
+        private void TextResY_PreviewTextInput( object sender, TextCompositionEventArgs e )
+        {
+            e.Handled = !ContainsDigitsOnly( e.Text );
+            base.OnPreviewTextInput( e );
+        }
+
+        private void TextResX_Pasting( object sender, DataObjectPastingEventArgs e )
+        {
+            if ( e.DataObject.GetDataPresent( typeof( string ) ) )
+            {
+                string Text = (string)e.DataObject.GetData( typeof( string ) );
+                if ( !ContainsDigitsOnly( Text ) )
+                {
+                    e.CancelCommand();
+                }
+            }
+            else
+            {
+                e.CancelCommand();
+            }
+        }
+
+        private void TextResY_Pasting( object sender, DataObjectPastingEventArgs e )
+        {
+            if ( e.DataObject.GetDataPresent( typeof( string ) ) )
+            {
+                string Text = (string)e.DataObject.GetData( typeof( string ) );
+                if ( !ContainsDigitsOnly( Text ) )
+                {
+                    e.CancelCommand();
+                }
+            }
+            else
+            {
+                e.CancelCommand();
+            }
+        }
+
+        void SetZoom( double NewZoom )
+        {
+            NewZoom = Math.Min( 6400.0, Math.Max( NewZoom, 3.13 ) );
+
+            if ( NewZoom != CurrentZoom )
+            {
+                double ResX, ResY;
+                double.TryParse( TextResX.Text, out ResX );
+                double.TryParse( TextResY.Text, out ResY );
+
+                if ( ResX != 0 && ResY != 0 )
+                {
+                    CurrentZoom = NewZoom;
+                    MainCanvas.Width = Math.Floor( ResX * CurrentZoom * 0.01 );
+                    MainCanvas.Height = Math.Floor( ResY * CurrentZoom * 0.01 );
+
+                    string ZoomText = string.Format( "{0:#####.##}", NewZoom ) + "%";
+
+                    bProcessZoomSelectionChanged = false;
+                    ZoomCombo.SelectedItem = (ComboBoxItem)ZoomCombo.Items
+                        .OfType<ComboBoxItem>()
+                        .Where( Item => Item.Content.ToString() == ZoomText ).FirstOrDefault();
+
+                    ZoomCombo.Text = ZoomText;
+                    bProcessZoomSelectionChanged = true;
+                }
+            }
+        }
+
+        void ParseZoom(string Text, out double NewZoom)
+        {
+            NewZoom = CurrentZoom;
+
+            char[] ToTrim = { '%' };
+            string ZoomText = Text.Trim( ToTrim );
+
+            double ResX, ResY;
+            double.TryParse( TextResX.Text, out ResX );
+            double.TryParse( TextResY.Text, out ResY );
+
+            if ( ResX != 0 && ResY != 0 )
+            {
+                bool bZoomFitAll = ZoomText == "Fit All";
+                if ( bZoomFitAll )
+                {
+                    double WidthScale = ScrollView.ActualWidth / ResX;
+                    double HeightScale = ScrollView.ActualHeight / ResY;
+                    NewZoom = (float)Math.Min( HeightScale, WidthScale ) * 100.0f;
+                }
+                else if ( !double.TryParse( ZoomText, out NewZoom ) )
+                {
+                    NewZoom = CurrentZoom;
+                }
+            }
+        }
+
+        private void ZoomCombo_KeyUp( object sender, KeyEventArgs e )
+        {
+            base.OnKeyUp( e );
+
+            if ( e.Key == Key.Return || e.Key == Key.Enter )
+            {
+                double NewZoom;
+                ParseZoom( ZoomCombo.Text, out NewZoom );
+                SetZoom( NewZoom );
+                Keyboard.ClearFocus();
+            }
+            else if ( e.Key == Key.Escape )
+            {
+                Keyboard.ClearFocus();
+            }
+        }
+
+        double GetNextZoomLevel( double Zoom, int Direction )
+        {
+            double NextZoom = 100.0;
+            int Steps = 0;
+
+            Action<bool> GetPrev = (bool bCheckEqual) =>
+            {
+                double[] ZoomMult = new double[ 2 ] { 2.0 / 3.0, 0.75 };
+                while ( NextZoom > Zoom || ( bCheckEqual && NextZoom == Zoom ) )
+                {
+                    bool bPreInc = Zoom > 100.0;
+                    if ( bPreInc )
+                    {
+                        Steps--;
+                    }
+                    NextZoom = NextZoom * ZoomMult[ Steps & 1 ];
+                    if ( !bPreInc )
+                    {
+                        Steps--;
+                    }
+                }
+            };
+
+            Action<bool> GetNext = (bool bCheckEqual) =>
+            {
+                double[] ZoomMult = new double[ 2 ] { 1.5, 1.0 + 1.0 / 3.0 };
+                while ( NextZoom < Zoom || ( bCheckEqual && NextZoom == Zoom ) )
+                {
+                    bool bPreInc = Zoom < 100.0;
+                    if ( bPreInc )
+                    {
+                        Steps++;
+                    }
+                    NextZoom = NextZoom * ZoomMult[ Steps & 1 ];
+                    if ( !bPreInc )
+                    {
+                        Steps++;
+                    }
+                }
+            };
+
+            if ( Direction > 0 )
+            {
+                GetNext( false );
+                GetPrev( false );
+                GetNext( true );
+            }
+            else
+            {
+                GetPrev( false );
+                GetNext( false );
+                GetPrev( true );
+            }
+
+            return NextZoom;
+        }
+
+        private void ScrollView_MouseWheel( object sender, MouseWheelEventArgs e )
+        {
+            if ( ( Keyboard.Modifiers & ModifierKeys.Control ) == ModifierKeys.Control )
+            {
+                double NewZoom = GetNextZoomLevel( CurrentZoom, Math.Sign( e.Delta ) );
+                SetZoom( NewZoom );
+
+                e.Handled = true;
+            }
+        }
+
+        private void ZoomCombo_SelectionChanged( object sender, SelectionChangedEventArgs e )
+        {
+            if ( bProcessZoomSelectionChanged )
+            {
+                ComboBoxItem Selected = e.AddedItems.OfType<ComboBoxItem>().FirstOrDefault();
+                if ( Selected != null )
+                {
+                    double NewZoom;
+                    ParseZoom( Selected.Content.ToString(), out NewZoom );
+                    SetZoom( NewZoom );
+                }
+                Keyboard.ClearFocus();
+            }
+        }
+
+        private void ZoomCombo_LostKeyboardFocus( object sender, KeyboardFocusChangedEventArgs e )
+        {
+            string ZoomText = string.Format( "{0:#####.##}", CurrentZoom ) + "%";
+            ZoomCombo.Text = ZoomText;
+
+            Action DelayUpdateText = () =>
+            {
+                ZoomCombo.Text = ZoomText;
+            };
+            ZoomCombo.Dispatcher.BeginInvoke( DelayUpdateText, DispatcherPriority.Send );
         }
     }
 }
